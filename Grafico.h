f330c957fde73704b9e5cb66720c5176fc16c835
Sofilau08 <113152152+Sofilau08@users.noreply.github.com>
2022-09-12 16:41:24 -1200

    Add files via upload

---
+++ Grafico.h
@@ -0,0 +1,22 @@
+#pragma once
+#include"CImg.h"
+int const n = 200;
+using namespace cimg_library;
+
+class Grafico
+{
+public:
+	Grafico(); //constructor
+
+	//método para pintar usando la biblioteca CImg
+	void pintar(CImg<unsigned char>& imagen);
+	void calcularSeno();
+private:
+
+	//Ubicación en la ventana de la coordenada 0,0 (x,y):
+	int ubicX0, ubicY0;
+
+	//valores de Y.
+	int valores_Y[n];
+};
+
